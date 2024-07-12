7d3426219b39771d495a7f020d090c5a98a49b2e
raksha-rathore <official.raksha.singh@gmail.com>
2024-07-12 21:17:22 +0530

    directory structure updated

---
+++ container/webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+
+    devServer: {
+        port: 8082
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
---
+++ products/README.md
@@ -0,0 +1,2 @@
+# micro-frontend-using-webpack
+micro front end using webpack 5
